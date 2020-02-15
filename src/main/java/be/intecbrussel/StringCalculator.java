aa52ab77a65b279ed836d04757719f0b1933ef93
caltheblaze <cal.van.paesschen@gmail.com>
2020-02-15 14:32:28 +0100

    need to step away from this for a bit

--- src/main/java/be/intecbrussel/StringCalculator.java
+++ src/main/java/be/intecbrussel/StringCalculator.java
@@ -43,16 +43,18 @@ public class StringCalculator {
 
     private String replaceDelimiters(String numbers, ArrayList<String> delimiters) {
         numbers = numbers.replace("//", "");
+        String temp = "";
         for (String s : delimiters) {
-            if (s.length() != 1) {
+            if (s.length() > 1) {
+                //split the multi symbol delimiters in single character strings
                 String[] tempArray = s.split("");
-                String temp = "";
                 for (String t : tempArray) {
+                    //put every character with a backslash in front of it to filter
                     temp += String.format("\\%s", t);
                 }
                 s = temp;
-            } else { //can't seem to make it work for singular characters [.]
-
+            } else {
+                //TODO single metacharacter characters
             }
             numbers = numbers.replaceAll(String.format("\\%s", s), " ");
         }
@@ -81,13 +83,12 @@ public class StringCalculator {
                 tempDelimiter = tempNumbers.substring(tempNumbers.indexOf('['), tempNumbers.indexOf(']') + 1);
                 //remove the [delimiter]
                 tempNumbers = tempNumbers.replace(tempDelimiter, "");
-                //add the delimiter without the [ ] to the array
-                delimiters.add( String.format("%s%s",
+                //add the delimiter without the [ ] to the ArrayList
+                delimiters.add(String.format("%s%s",
                         tempDelimiter.length()==3?"\\":"",
                         tempDelimiter.substring(1, tempDelimiter.length() - 2)));
             } while (tempNumbers.contains("["));
         }
-
         return replaceDelimiters(tempNumbers, delimiters);
     }
 
