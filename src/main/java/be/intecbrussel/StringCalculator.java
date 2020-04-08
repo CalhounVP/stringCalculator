f0b469aa1231da195c257a421239690fe9613d01
caltheblaze <cal.van.paesschen@gmail.com>
2020-04-08 20:52:03 +0200

    redid step 4

--- src/main/java/be/intecbrussel/StringCalculator.java
+++ src/main/java/be/intecbrussel/StringCalculator.java
@@ -7,18 +7,29 @@ public class StringCalculator {
 
     public int add(String numbers) {
         int sum = 0;
+        String delimiter = "[,\\n]";
 
         if (!emptyOrNullString(numbers)) {
-            for (String split : numbers.split("[,\\n]")) {
+            if (numbers.startsWith("//")) {
+                String[] splitInput = numbers.split("\\n");
+                delimiter = settingCustomDelimiters(splitInput[0]);
+                numbers = splitInput[1];
+            }
+
+            for (String split : numbers.split(delimiter)) {
                 sum += Integer.parseInt(split.trim());
             }
         }
-        
+
         return sum;
     }
 
     public boolean emptyOrNullString(String numbers) {
         return numbers == null || numbers.isEmpty();
+    }
+
+    public String settingCustomDelimiters (String delimiters) {
+        return delimiters.replaceFirst("//", "");
     }
 
 }
